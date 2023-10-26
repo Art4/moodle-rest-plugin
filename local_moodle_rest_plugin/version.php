4c8f4f0c75269c8348d26054e025a7df8c1c332f
Art4 <art4@wlabs.de>
2023-10-26 23:20:19 +0200

    Create version 0.1.1 to test updates

--- local_moodle_rest_plugin/version.php
+++ local_moodle_rest_plugin/version.php
@@ -12,19 +12,16 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var \stdClass $plugin */
 $plugin->component = 'local_moodle_rest_plugin';
-$plugin->version = 2023102600;
-$plugin->release = '0.1.0';
+$plugin->version = 2023102601;
+$plugin->release = '0.1.1';
 
 $plugin->requires = 2023100900.00; // Moodle 4.3
 // Available as of Moodle 3.9.0 or later.
 $plugin->supported = [
-    403,
+    402,
     403,
 ];
 $plugin->incompatible = 404; // Available as of Moodle 3.9.0 or later.
 $plugin->maturity = MATURITY_ALPHA; // one of: MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE
 
-$plugin->dependencies = [
-    // 'mod_forum' => 2022042100,
-    // 'mod_data' => 2022042100
-];
+$plugin->dependencies = [];
